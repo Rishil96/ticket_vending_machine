928969220d3c3da993b026da7e48aaabe975a5f5
Rishil Ramesh <rishil.ramesh@outlook.com>
2022-11-19 12:55:47 +0530

    Add : Add decorators to the generator functions

---
+++ generator_functions.py
@@ -0,0 +1,58 @@
+from os import system
+
+PERFUME_TICKET_NUMBER = -1
+MEDICINE_TICKET_NUMBER = -1
+COSMETICS_TICKET_NUMBER = -1
+
+
+# Check if user input is valid
+def is_input_valid(user_input):
+    return user_input in ['1', '2', '3', '4']
+
+
+# Clear screen
+def clear_screen():
+    system("cls")
+
+
+# Decorate Ticket
+def ticket_message(function):
+    def wrapper():
+        print("\nYour number is:-")
+        print(next(function()))
+        print("wait and someone will be with you shortly.\n")
+
+    return wrapper
+
+
+# Ticket generator for cosmetics
+@ticket_message
+def get_cosmetics_ticket():
+    global COSMETICS_TICKET_NUMBER
+    COSMETICS_TICKET_NUMBER += 1
+    while True:
+        yield f"C-{COSMETICS_TICKET_NUMBER}"
+
+
+# Ticket generator for medicines
+@ticket_message
+def get_medicine_ticket():
+    global MEDICINE_TICKET_NUMBER
+    while True:
+        MEDICINE_TICKET_NUMBER += 1
+        yield f"M-{MEDICINE_TICKET_NUMBER}"
+
+
+# Ticket generator for perfume
+@ticket_message
+def get_perfume_ticket():
+    global PERFUME_TICKET_NUMBER
+    while True:
+        PERFUME_TICKET_NUMBER += 1
+        yield f"P-{PERFUME_TICKET_NUMBER}"
+
+
+# Go back to home screen
+def go_back():
+    user_input = input("\nPress any button to go back to the home screen:- ")
+    return bool(user_input)
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # Ticket Vending Machine
-import numbers
+import generator_functions
 import time
 
-perfume_ticket = numbers.get_perfume_ticket()
-medicine_ticket = numbers.get_medicine_ticket()
-cosmetics_ticket = numbers.get_cosmetics_ticket()
+perfume_ticket = generator_functions.get_perfume_ticket()
+medicine_ticket = generator_functions.get_medicine_ticket()
+cosmetics_ticket = generator_functions.get_cosmetics_ticket()
 
 
 def main():
@@ -14,31 +14,31 @@ def main():
 
     while is_open:
 
-        numbers.clear_screen()
+        generator_functions.clear_screen()
 
         user_input = input("Which part of the store do you want to visit:-"
                            "\n[1] Perfumes.\n[2] Medicines.\n[3] Cosmetics.\n[4] Close Store"
                            "\nEnter your input: ")
 
-        if not numbers.is_input_valid(user_input):
+        if not generator_functions.is_input_valid(user_input):
             print("Invalid Input. Try again in 3 seconds!")
             time.sleep(3)
             continue
 
         if user_input == "1":
-            print("Welcome to Perfume Store")
-            print(next(perfume_ticket))
-            numbers.go_back()
+            print("Welcome to Perfume Store.")
+            generator_functions.get_perfume_ticket()
+            generator_functions.go_back()
 
         elif user_input == "2":
-            print("Welcome to Medicine Store")
-            print(next(medicine_ticket))
-            numbers.go_back()
+            print("Welcome to Medicine Store.")
+            generator_functions.get_medicine_ticket()
+            generator_functions.go_back()
 
         elif user_input == "3":
-            print("Welcome to Cosmetics Store")
-            print(next(cosmetics_ticket))
-            numbers.go_back()
+            print("Welcome to Cosmetics Store.")
+            generator_functions.get_cosmetics_ticket()
+            generator_functions.go_back()
 
         else:
             print("The store is now closed. Come back again tomorrow :)")
